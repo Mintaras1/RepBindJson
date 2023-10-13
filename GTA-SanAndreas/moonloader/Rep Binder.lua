09df71da9c0d09e05d6467153cd1b8d6877377d1
Mintaras1 <45083916+Mintaras1@users.noreply.github.com>
2023-10-13 13:00:11 +0300

    Update Rep Binder.lua
    rep bind setuper

--- GTA-SanAndreas/moonloader/Rep Binder.lua
+++ GTA-SanAndreas/moonloader/Rep Binder.lua
@@ -1,37 +1,82 @@
 --[[
     Setup utility for Rep Binder
-
+    
     > Sukuria papkes
-    > Parsiunčia "imgui" lib'ą
-    > Parsiunčia "Rep Binder.luac" Stable versija.
+    > Parsiunčia "imgui" lib'ą 
+    > Parsiunčia "SampEvents" lib'ą 
+    > Parsiunčia "AutoReboot.lua", "SF Integration.lua", "reload_all.lua" papildinius 
+    > Parsiunčia "Rep Binder.luac" Stable versija. 
 
-    @Mintaras - 2023-10-12
+    @Mintaras - 2023-10-13 
 ]]
 
 local dlstatus = require('moonloader').download_status 
 
 function main()
     repeat wait(0) until isSampAvailable() 
+    repeat wait(0) until sampIsLocalPlayerSpawned() 
+
     if not doesDirectoryExist(thisScript().directory.."/lib/mimgui") then createDirectory(thisScript().directory.."/lib/mimgui") end 
+    if not doesDirectoryExist(thisScript().directory.."/lib/samp") then createDirectory(thisScript().directory.."/lib/samp") end 
+    if not doesDirectoryExist(thisScript().directory.."/lib/samp/events") then createDirectory(thisScript().directory.."/lib/samp/events") end 
     if not doesDirectoryExist(thisScript().directory.."/config/RepBind") then createDirectory(thisScript().directory.."/config/RepBind") end 
 
-    if not doesFileExist(thisScript().directory.."/lib/imgui.lua") then print("Downloading: ./GTA-SanAndreas/lib/imgui.lua",-1) downloadUrlToFile("https://raw.githubusercontent.com/Mintaras1/RepBindJson/main/GTA-SanAndreas/moonloader/lib/imgui.lua", thisScript().directory.."/lib/imgui.lua") end 
+    if not doesFileExist(thisScript().directory.."/lib/imgui.lua") then print("Downloading: ./GTA-SanAndreas/moonloader/lib/imgui.lua") downloadUrlToFile("https://raw.githubusercontent.com/Mintaras1/RepBindJson/main/GTA-SanAndreas/moonloader/lib/imgui.lua", thisScript().directory.."/lib/imgui.lua") end 
     while not doesFileExist(thisScript().directory.."/lib/imgui.lua") do wait(1) end 
 
-	if not doesFileExist(thisScript().directory.."/lib/mimgui/cdefs.lua") then print("/Downloading: ./GTA-SanAndreas/lib/mimgui/cdefs.lua",-1) downloadUrlToFile("https://raw.githubusercontent.com/Mintaras1/RepBindJson/main/GTA-SanAndreas/moonloader/lib/mimgui/cdefs.lua", thisScript().directory.."/lib/mimgui/cdefs.lua") end 
+	if not doesFileExist(thisScript().directory.."/lib/mimgui/cdefs.lua") then print("/Downloading: ./GTA-SanAndreas/moonloader/lib/mimgui/cdefs.lua") downloadUrlToFile("https://raw.githubusercontent.com/Mintaras1/RepBindJson/main/GTA-SanAndreas/moonloader/lib/mimgui/cdefs.lua", thisScript().directory.."/lib/mimgui/cdefs.lua") end 
     while not doesFileExist(thisScript().directory.."/lib/mimgui/cdefs.lua") do wait(1) end 
 
-	if not doesFileExist(thisScript().directory.."/lib/mimgui/cimguidx9.dll") then print("/Downloading: ./GTA-SanAndreas/lib/mimgui/cimguidx9.dll",-1) downloadUrlToFile("https://github.com/Mintaras1/RepBindJson/raw/main/GTA-SanAndreas/moonloader/lib/mimgui/cimguidx9.dll", thisScript().directory.."/lib/mimgui/cimguidx9.dll") end
+	if not doesFileExist(thisScript().directory.."/lib/mimgui/cimguidx9.dll") then print("/Downloading: ./GTA-SanAndreas/moonloader/lib/mimgui/cimguidx9.dll") downloadUrlToFile("https://github.com/Mintaras1/RepBindJson/raw/main/GTA-SanAndreas/moonloader/lib/mimgui/cimguidx9.dll", thisScript().directory.."/lib/mimgui/cimguidx9.dll") end
     while not doesFileExist(thisScript().directory.."/lib/mimgui/cimguidx9.dll") do wait(1) end  
 
-	if not doesFileExist(thisScript().directory.."/lib/mimgui/dx9.lua") then print("/Downloading: ./GTA-SanAndreas/lib/mimgui/dx9.lua",-1) downloadUrlToFile("https://github.com/Mintaras1/RepBindJson/raw/main/GTA-SanAndreas/moonloader/lib/mimgui/dx9.lua", thisScript().directory.."/lib/mimgui/dx9.lua") end 
+	if not doesFileExist(thisScript().directory.."/lib/mimgui/dx9.lua") then print("/Downloading: ./GTA-SanAndreas/moonloader/lib/mimgui/dx9.lua") downloadUrlToFile("https://github.com/Mintaras1/RepBindJson/raw/main/GTA-SanAndreas/moonloader/lib/mimgui/dx9.lua", thisScript().directory.."/lib/mimgui/dx9.lua") end 
     while not doesFileExist(thisScript().directory.."/lib/mimgui/dx9.lua") do wait(1) end 
 
-	if not doesFileExist(thisScript().directory.."/lib/mimgui/imgui.lua") then print("/Downloading: ./GTA-SanAndreas/lib/mimgui/imgui.lua",-1) downloadUrlToFile("https://github.com/Mintaras1/RepBindJson/raw/main/GTA-SanAndreas/moonloader/lib/mimgui/imgui.lua", thisScript().directory.."/lib/mimgui/imgui.lua") end 
+	if not doesFileExist(thisScript().directory.."/lib/mimgui/imgui.lua") then print("/Downloading: ./GTA-SanAndreas/moonloader/lib/mimgui/imgui.lua") downloadUrlToFile("https://github.com/Mintaras1/RepBindJson/raw/main/GTA-SanAndreas/moonloader/lib/mimgui/imgui.lua", thisScript().directory.."/lib/mimgui/imgui.lua") end 
     while not doesFileExist(thisScript().directory.."/lib/mimgui/imgui.lua") do wait(1) end 
 
-	if not doesFileExist(thisScript().directory.."/lib/mimgui/init.lua") then print("/Downloading: ./GTA-SanAndreas/lib/mimgui/init.lua",-1) downloadUrlToFile("https://github.com/Mintaras1/RepBindJson/raw/main/GTA-SanAndreas/moonloader/lib/mimgui/init.lua", thisScript().directory.."/lib/mimgui/init.lua") end 
+	if not doesFileExist(thisScript().directory.."/lib/mimgui/init.lua") then print("/Downloading: ./GTA-SanAndreas/moonloader/lib/mimgui/init.lua") downloadUrlToFile("https://github.com/Mintaras1/RepBindJson/raw/main/GTA-SanAndreas/moonloader/lib/mimgui/init.lua", thisScript().directory.."/lib/mimgui/init.lua") end 
     while not doesFileExist(thisScript().directory.."/lib/mimgui/init.lua") do wait(1) end 
+
+    --
+
+	if not doesFileExist(thisScript().directory.."/lib/samp/events.lua") then print("/Downloading: ./GTA-SanAndreas/moonloader/lib/samp/events.lua") downloadUrlToFile("https://github.com/Mintaras1/RepBindJson/raw/main/GTA-SanAndreas/moonloader/lib/samp/events.lua", thisScript().directory.."/lib/samp/events.lua") end 
+    while not doesFileExist(thisScript().directory.."/lib/samp/events.lua") do wait(1) end 
+
+	if not doesFileExist(thisScript().directory.."/lib/samp/raknet.lua") then print("/Downloading: ./GTA-SanAndreas/moonloader/lib/samp/raknet.lua") downloadUrlToFile("https://github.com/Mintaras1/RepBindJson/raw/main/GTA-SanAndreas/moonloader/lib/samp/raknet.lua", thisScript().directory.."/lib/samp/raknet.lua") end 
+    while not doesFileExist(thisScript().directory.."/lib/samp/raknet.lua") do wait(1) end 
+
+	if not doesFileExist(thisScript().directory.."/lib/samp/synchronization.lua") then print("/Downloading: ./GTA-SanAndreas/moonloader/lib/samp/synchronization.lua") downloadUrlToFile("https://github.com/Mintaras1/RepBindJson/raw/main/GTA-SanAndreas/moonloader/lib/samp/synchronization.lua", thisScript().directory.."/lib/samp/synchronization.lua") end 
+    while not doesFileExist(thisScript().directory.."/lib/samp/synchronization.lua") do wait(1) end 
+
+	if not doesFileExist(thisScript().directory.."/lib/samp/events/bitstream_io.lua") then print("/Downloading: ./GTA-SanAndreas/moonloader/lib/samp/events/bitstream_io.lua") downloadUrlToFile("https://github.com/Mintaras1/RepBindJson/raw/main/GTA-SanAndreas/moonloader/lib/samp/events/bitstream_io.lua", thisScript().directory.."/lib/samp/events/bitstream_io.lua") end 
+    while not doesFileExist(thisScript().directory.."/lib/samp/events/bitstream_io.lua") do wait(1) end 
+
+	if not doesFileExist(thisScript().directory.."/lib/samp/events/core.lua") then print("/Downloading: ./GTA-SanAndreas/moonloader/lib/samp/events/core.lua") downloadUrlToFile("https://github.com/Mintaras1/RepBindJson/raw/main/GTA-SanAndreas/moonloader/lib/samp/events/core.lua", thisScript().directory.."/lib/samp/events/core.lua") end 
+    while not doesFileExist(thisScript().directory.."/lib/samp/events/core.lua") do wait(1) end 
+
+	if not doesFileExist(thisScript().directory.."/lib/samp/events/extra_types.lua") then print("/Downloading: ./GTA-SanAndreas/moonloader/lib/samp/events/extra_types.lua") downloadUrlToFile("https://github.com/Mintaras1/RepBindJson/raw/main/GTA-SanAndreas/moonloader/lib/samp/events/extra_types.lua", thisScript().directory.."/lib/samp/events/extra_types.lua") end 
+    while not doesFileExist(thisScript().directory.."/lib/samp/events/extra_types.lua") do wait(1) end 
+
+	if not doesFileExist(thisScript().directory.."/lib/samp/events/handlers.lua") then print("/Downloading: ./GTA-SanAndreas/moonloader/lib/samp/events/handlers.lua") downloadUrlToFile("https://github.com/Mintaras1/RepBindJson/raw/main/GTA-SanAndreas/moonloader/lib/samp/events/handlers.lua", thisScript().directory.."/lib/samp/events/handlers.lua") end 
+    while not doesFileExist(thisScript().directory.."/lib/samp/events/handlers.lua") do wait(1) end 
+
+	if not doesFileExist(thisScript().directory.."/lib/samp/events/utils.lua") then print("/Downloading: ./GTA-SanAndreas/moonloader/lib/samp/events/utils.lua") downloadUrlToFile("https://github.com/Mintaras1/RepBindJson/raw/main/GTA-SanAndreas/moonloader/lib/samp/events/utils.lua", thisScript().directory.."/lib/samp/events/utils.lua") end 
+    while not doesFileExist(thisScript().directory.."/lib/samp/events/utils.lua") do wait(1) end 
+
+    --
+
+	if not doesFileExist(thisScript().directory.."/AutoReboot.lua") then print("/Downloading: ./GTA-SanAndreas/moonloader/AutoReboot.lua") downloadUrlToFile("https://github.com/Mintaras1/RepBindJson/raw/main/GTA-SanAndreas/moonloader/AutoReboot.lua", thisScript().directory.."/AutoReboot.lua") end 
+    while not doesFileExist(thisScript().directory.."/AutoReboot.lua") do wait(1) end 
+
+	if not doesFileExist(thisScript().directory.."/SF Integration.lua") then print("/Downloading: ./GTA-SanAndreas/moonloader/SF Integration.lua") downloadUrlToFile("https://github.com/Mintaras1/RepBindJson/raw/main/GTA-SanAndreas/moonloader/SF Integration.lua", thisScript().directory.."/SF Integration.lua") end 
+    while not doesFileExist(thisScript().directory.."/SF Integration.lua") do wait(1) end 
+
+	if not doesFileExist(thisScript().directory.."/reload_all.lua") then print("/Downloading: ./GTA-SanAndreas/moonloader/reload_all.lua") downloadUrlToFile("https://github.com/Mintaras1/RepBindJson/raw/main/GTA-SanAndreas/moonloader/reload_all.lua", thisScript().directory.."/reload_all.lua") end 
+    while not doesFileExist(thisScript().directory.."/reload_all.lua") do wait(1) end 
+
+    --
 
     sampAddChatMessage(ltu("{dF8D1B}[RepBind.lua] {e6BB24}Visi reikalingi failai buvo atsiūsti."), 0xe6BB24)
 	downloadUrlToFile("https://raw.githubusercontent.com/Mintaras1/RepBindJson/main/global.json", thisScript().directory.."/config/RepBind/global.json", function(id, status, p1, p2) 
@@ -50,7 +95,7 @@ function main()
                             if doesFileExist(thisScript().directory.."AutoReboot.lua") then 
                                 thisScript():unload() 
                             else 
-                                thisScript():reload() 
+                                reloadScripts()
                             end 
                         end) 
                     end 
